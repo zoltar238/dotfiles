b2dd641c276c561ebfac65848e234338067bfe82
andrei <protoandrei@protonmail.com>
2026-06-17 09:38:57 +0200

    Changed default terminal, from kitty -> wezterm

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -13,100 +13,102 @@ local programs = require("programs")
 ---- KEYBINDINGS ----
 require("bindings")
 
+---- ENV ----
+hl.env("XDG_CURRENT_DESKTOP", "Hyprland")
+
 ---- AUTOSTART ----
-hl.on("hyprland.start", function ()
-    hl.exec_cmd("waybar")
-    hl.exec_cmd("hyprpaper")
-    hl.exec_cmd("hypridle")
-    hl.exec_cmd("mako")
-    -- Delete pacamn cache keeping latest3 versions
-    hl.exec_cmd("sudo paccache -rk1")
-    -- Launch my most used apps
-    hl.exec_cmd(programs.fileManager, { workspace = "8", no_initial_focus = true, monitor = "DP-2" })
-    hl.exec_cmd(programs.pycharm, { workspace = "6", no_initial_focus = true, monitor = "DP-1" })
-    hl.exec_cmd(programs.browser, { workspace = "6", no_initial_focus = true, monitor = "DP-2" })
-    hl.exec_cmd(programs.whatsapp, { workspace = "9", no_initial_focus = true, monitor = "DP-2" })
-    hl.exec_cmd(programs.flameshot, { workspace = "9", no_initial_focus = true, monitor = "DP-2" })
-    hl.exec_cmd(programs.discord, { workspace = "9", no_initial_focus = true, monitor = "DP-2" })
-    -- Wait for internet connection before opening spotify to avoid errors
-    hl.exec_cmd(
-    "sh -c 'until ping -c 1 -W 1 1.1.1.1 >/dev/null 2>&1; do sleep 1; done; exec " .. programs.spotify .. "'",
-    { workspace = "10", no_initial_focus = true, monitor = "DP-2" }
-)
+hl.on("hyprland.start", function()
+	hl.exec_cmd("waybar")
+	hl.exec_cmd("hyprpaper")
+	hl.exec_cmd("hypridle")
+	hl.exec_cmd("mako")
+	-- Delete pacamn cache keeping latest3 versions
+	hl.exec_cmd("sudo paccache -rk1")
+	-- Launch my most used apps
+	hl.exec_cmd(programs.fileManager, { workspace = "8", no_initial_focus = true, monitor = "DP-2" })
+	hl.exec_cmd(programs.pycharm, { workspace = "6", no_initial_focus = true, monitor = "DP-1" })
+	hl.exec_cmd(programs.browser, { workspace = "6", no_initial_focus = true, monitor = "DP-2" })
+	hl.exec_cmd(programs.whatsapp, { workspace = "9", no_initial_focus = true, monitor = "DP-2" })
+	hl.exec_cmd(programs.flameshot, { workspace = "9", no_initial_focus = true, monitor = "DP-2" })
+	hl.exec_cmd(programs.discord, { workspace = "9", no_initial_focus = true, monitor = "DP-2" })
+	-- Wait for internet connection before opening spotify to avoid errors
+	hl.exec_cmd(
+		"sh -c 'until ping -c 1 -W 1 1.1.1.1 >/dev/null 2>&1; do sleep 1; done; exec " .. programs.spotify .. "'",
+		{ workspace = "10", no_initial_focus = true, monitor = "DP-2" }
+	)
 end)
 
 hl.window_rule({
-    name = "spotify",
-    match = {
-        class = "Spotify"
-    },
-    workspace = "10",
-    no_initial_focus = true,
-    no_follow_mouse = true,
+	name = "spotify",
+	match = {
+		class = "Spotify",
+	},
+	workspace = "10",
+	no_initial_focus = true,
+	no_follow_mouse = true,
 })
 
 hl.window_rule({
-    name = "discord-rule",
-    match = {
-        --initial_class = "^discord$",
-        initial_class = "^vesktop$",
-    },
-    workspace = "9",
-    no_initial_focus = true,
+	name = "discord-rule",
+	match = {
+		--initial_class = "^discord$",
+		initial_class = "^vesktop$",
+	},
+	workspace = "9",
+	no_initial_focus = true,
 })
 
-
 hl.window_rule({
-    name = "discord",
-    match = {
-        class = "elecwhat"
-    },
-    workspace = "9",
-    no_initial_focus = true,
+	name = "discord",
+	match = {
+		class = "elecwhat",
+	},
+	workspace = "9",
+	no_initial_focus = true,
 })
 
 ---- LOOK AND FEEL ----
 -- Refer to https://wiki.hypr.land/Configuring/Basics/Variables/
 hl.config({
-    general = {
-        gaps_in = 5,
-        gaps_out = 20,
-        border_size = 3,
-        col = {
-            active_border = { colors = { "rgba(91338766)", "rgba(4f007499)" }, angle = 45 },
-            inactive_border = "rgba(595959aa)"
-        },
-        resize_on_border = true,
-        allow_tearing = true,
-        layout = "master"
-    },
-    decoration = {
-        rounding = 15,
-        rounding_power = 3,
-        active_opacity = 1.0,
-        inactive_opacity = 1.0,
-        shadow = {
-            enabled = true,
-            range = 19,
-            render_power = 3,
-            color = "rgba(0,0,0, 0.3)"
-        },
-        blur = {
-            enabled = true,
-            size = 4,
-            passes = 1,
-            vibrancy = 0.1696
-        },
-        glow = {
-            enabled = true,
-            color = "rgba(91338766)",
-            render_power = 4,
-            range = 15
-        }
-    },
-    animations = {
-        enabled = true
-    }
+	general = {
+		gaps_in = 5,
+		gaps_out = 20,
+		border_size = 3,
+		col = {
+			active_border = { colors = { "rgba(91338766)", "rgba(4f007499)" }, angle = 45 },
+			inactive_border = "rgba(595959aa)",
+		},
+		resize_on_border = true,
+		allow_tearing = true,
+		layout = "master",
+	},
+	decoration = {
+		rounding = 15,
+		rounding_power = 3,
+		active_opacity = 1.0,
+		inactive_opacity = 1.0,
+		shadow = {
+			enabled = true,
+			range = 19,
+			render_power = 3,
+			color = "rgba(0,0,0, 0.3)",
+		},
+		blur = {
+			enabled = true,
+			size = 4,
+			passes = 1,
+			vibrancy = 0.1696,
+		},
+		glow = {
+			enabled = true,
+			color = "rgba(91338766)",
+			render_power = 4,
+			range = 15,
+		},
+	},
+	animations = {
+		enabled = true,
+	},
 })
 
 -- Default curves and animations, see https://wiki.hypr.land/Configuring/Advanced-and-Cool/Animations/
--- hypr/programs.lua
+++ hypr/programs.lua
@@ -1,14 +1,14 @@
 --- Programs configurations
 local programs = {
-    terminal = "kitty $HOME",
-    fileManager = "nemo",
-    menu = "rofi -show drun",
-    browser = "zen-browser",
-    spotify = "spotify-launcher",
-    whatsapp = "elecwhat",
-    discord = "vesktop",
-    pycharm = "pycharm",
-    flameshot = "flameshot",
+	terminal = "wezterm",
+	fileManager = "nemo",
+	menu = "rofi -show drun",
+	browser = "zen-browser",
+	spotify = "spotify-launcher",
+	whatsapp = "elecwhat",
+	discord = "vesktop",
+	pycharm = "pycharm",
+	flameshot = "flameshot",
 }
 
 return programs
